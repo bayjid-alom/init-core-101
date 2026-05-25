c8757132009c44add3e740a752c35c79eea71444
bayjidalom <bayjidalom1005@gmail.com>
2026-05-25 10:13:55 +0600

    day-02: first program & syntax

---
+++ day_02.py
@@ -0,0 +1,12 @@
+# first program & syntax
+
+# string
+print("Hello world! Welcome to Python Programming!")
+print("Bayjid Alom Jihad")
+
+# integer (Not needed inverted comma)
+print(1000)
+
+# float
+print("2.0.0")
+print("3.1416")
