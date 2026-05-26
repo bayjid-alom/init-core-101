d63067b9f66cc1879c7d43d0635e9d39237a21d1
bayjidalom <bayjidalom1005@gmail.com>
2026-05-26 12:39:08 +0600

    day-04: modules & pip

---
+++ day_04.py
@@ -0,0 +1,41 @@
+# Module and Pip
+
+# Modules are pre-built by Python developers.
+# pip is a tool used to download and install Python libraries from the internet.
+
+"""
+🟦 Module কী?
+Module হলো Python-এর আলাদা .py file যেখানে code/function রাখা হয়, যেন পরে অন্য file থেকে use করা যায়।
+
+Python modules are primarily categorized into ---
+
+(i) Built-in modules
+(ii) User-defined modules and
+(iii) External (Third-Party) modules.
+
+Most used Built-in modules:
+math, random, datetime, os, sys, json, time, re, collections, statistics, itertools, functools, pathlib, logging, csv
+
+"""
+
+
+import math  # built-in
+
+num = 16
+print(math.sqrt(num))
+print(math.factorial(5))
+
+
+
+"""
+
+🟪 pip কী?
+pip হলো Python package installer।
+মানে internet থেকে অন্যদের তৈরি library/package install করতে ব্যবহার হয়।
+
+🟨 pip কী করে?
+✔ Internet থেকে code নিয়ে আসে
+✔ Python project এ add করে দেয়
+✔ তোমাকে ready-made feature use করতে দেয়
+
+"""
