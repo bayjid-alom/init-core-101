88a6440de3142d6469f9a61f058df1f70994ab09
bayjidalom <bayjidalom1005@gmail.com>
2026-05-26 12:19:57 +0600

    day-03: python comment

---
+++ day_03.py
@@ -0,0 +1,12 @@
+# This is an example of python comment.
+# You can write notes for yourself using comments
+
+print("Practice every day to improve your coding skills")
+
+
+"""
+This is also a python comment.
+This is multi-line comment.
+single line comment ---> #
+
+"""
