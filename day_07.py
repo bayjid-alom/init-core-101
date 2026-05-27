b13936503b4ca94b6c3e4db8fc59680eef2f2e5c
bayjidalom <bayjidalom1005@gmail.com>
2026-05-27 22:21:16 +0600

    day-07: data types - number

---
+++ day_07.py
@@ -0,0 +1,15 @@
+# Number (Data types)
+
+# integer number
+num_1 = 420
+print(num_1)
+print("Type of num1 :", type(num_1))
+
+# float number
+num_2 = 55.59
+print("Type of num2 :", type(num_2))
+
+
+# complex number (j)
+num_3 = 1835j
+print(type(num_3))   # <class 'complex'>
