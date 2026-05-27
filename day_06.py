d6f04a763743b11052a7e8e7ae58479bdc79ecca
bayjidalom <bayjidalom1005@gmail.com>
2026-05-27 16:49:11 +0600

    day-06: data types - string

---
+++ day_06.py
@@ -0,0 +1,16 @@
+# Data types
+"""
+Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType
+"""
+
+# string /text type
+school = "Rajibpur Aftab Uddin High School"
+print("School Name :", school)
+
