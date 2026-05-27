45c130ba0a9aadf75b215dd60d9c91709f99ccfc
bayjidalom <bayjidalom1005@gmail.com>
2026-05-27 16:41:51 +0600

    day-05: variable

---
+++ day_05.py
@@ -0,0 +1,9 @@
+# Varibale (A place of computer memory)
+
+name = "Md. Bayjid Alom Jihad"
+number = 100
+message = "Welcome to python programming."
+
+print("Name :", name)
+print(number)
+print("Message :", message)
