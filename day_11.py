959610a9869f626987c08196a77f6104676ea213
bayjidalom <bayjidalom1005@gmail.com>
2026-05-29 21:20:09 +0600

    day-11: None type data

---
+++ day_11.py
@@ -0,0 +1,12 @@
+# None type
+
+"""
+In Python, None is a special constant used to represent the absence of a value or a null state.
+Its data type is NoneType.
+"""
+
+x = None
+print(type(x))     #<class 'NoneType'>
+
+# when we use it?
+# When you want to create a variable but don’t want to assign a real value now.
