0813742ee01a8845ce2db7925ebf8510933cb6e5
bayjidalom <bayjidalom1005@gmail.com>
2026-05-31 00:20:57 +0600

    day-15: swapping

---
+++ day_15.py
@@ -0,0 +1,14 @@
+# Swapping
+# Value Xchange
+
+"""
+
+The most efficient and Pythonic way to swap two variables is using a single line of code: a, b = b, a.
+"""
+
+a = 50
+b = 100
+
+a , b = b , a
+print("Value of a :", a)
+print("Value of b :", b)
