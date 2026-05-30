cf620765a489497be96c5afb01918755ba7b2ce7
bayjidalom <bayjidalom1005@gmail.com>
2026-05-31 00:25:34 +0600

    day-16: user input

---
+++ day_16.py
@@ -0,0 +1,13 @@
+# User input
+
+A = int(input("Enter the value of A : "))
+B = int(input("Enter the value of B : "))
+summation = A + B
+print("Summation is :", summation)
+
+
+username = input("Enter username : ")
+password = input("Enter password : ")
+print("Username is :", username)
+print("Password is :", password)
+
