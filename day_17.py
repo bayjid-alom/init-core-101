bf76594e5d68d32ca9f171c5ed0f9860cfa57923
bayjidalom <bayjidalom1005@gmail.com>
2026-05-31 00:33:24 +0600

    day-17: type casting

---
+++ day_17.py
@@ -0,0 +1,23 @@
+# What is type Casting in Python?
+"""
+Type casting in Python is the process of converting a variable from one data type to another.
+
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+
+"""
+# float to integer
+number_1 = 20.5
+print("Integer of 20.5 :", int(number_1))
+
+# integer to float
+number_2 = 55
+print("Float of 55 :", float(number_2))
+
+
+# string to float
+number_3 = "50"
+print("String to number :", float(number_3))
+
