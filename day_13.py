3590bdcaae764cd2c67175ca68a51fa2ec5e8852
bayjidalom <bayjidalom1005@gmail.com>
2026-05-30 23:39:01 +0600

    day-13: arithmetic operators

---
+++ day_13.py
@@ -0,0 +1,64 @@
+# Operators
+"""
+Arithmetic operators
+Assignment operators
+Comparison operators
+Logical operators
+Identity operators
+Membership operators
+Bitwise operators
+"""
+
+
+# Arithmetic Operators
+"""
+Arithmetic Operators
+Arithmetic operators are used with numeric values to perform common mathematical operations:
+
+Operator	Name	Example	Try it
++	Addition	x + y	
+-	Subtraction	x - y	
+*	Multiplication	x * y	
+/	Division	x / y	
+%	Modulus	x % y	
+**	Exponentiation	x ** y	
+//	Floor division	x // y
+"""
+
+# Addition
+a = 25
+b = 5
+print("Addition :", a + b)
+
+# Subtraction
+print("Subtraction :", a - b)
+
+# Multiplication
+print("Multiplication :", a * b)
+
+
+# Division
+print("Division :", a / b)
+
+
+# Modulus
+print("Modulus :", a % b)
+
+
+# Exponentiation
+x = 5
+y = 3
+print("Exponentiation of 5 :", x ** y)    # 125
+
+m = 10
+n = 3
+print("Exponentiation of 10 :", m % n)
+
+
+
+# Floor division
+# 12.5 ---> 12
+q = 25
+r = 2
+print("Floor division :", q // r)
+
