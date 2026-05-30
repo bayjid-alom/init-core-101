9cdf18b6ac80321195091dc3676314b327fa7ab3
bayjidalom <bayjidalom1005@gmail.com>
2026-05-30 23:55:15 +0600

    day-14: arithmetic & logical operators

---
+++ day_14.py
@@ -0,0 +1,64 @@
+# Assignment Operator (=)
+
+"""
+Assignment Operators
+Assignment operators are used to assign values to variables:
+
+Operator	Example	        Same As	Try it
+=	         x = 5	        x = 5
++=	         x += 3	        x = x + 3
+-=	         x -= 3	        x = x - 3
+*=	         x *= 3	        x = x * 3
+/=	         x /= 3	        x = x / 3
+%=	         x %= 3	        x = x % 3
+//=	         x //= 3	    x = x // 3
+**=	         x **= 3	    x = x ** 3
+&=	         x &= 3	        x = x & 3
+|=	         x |= 3	        x = x | 3
+^=	         x ^= 3	        x = x ^ 3
+>>=	         x >>= 3	    x = x >> 3
+<<=	         x <<= 3	    x = x << 3
+:=	        print(x := 3)   x = 3
+
+"""
+
+# Addition
+a = 100
+sum = a + 50     # way - 01
+sum += 50        # way - 02    #shortcut
+print(sum)
+
+
+# Subtraction
+x = 50
+x -= 25
+print("After Subtraction:", x)
+
+
+# Multiply
+m = 10
+m *= 10
+print("After Multiplication:", m)
+
+
+
+
+
+# Comparison Operators
+"""
+==   	Equal	                        x == y	
+!=	    Not equal                   	x != y	
+>	    Greater than                	x > y	
+<	    Less than	                    x < y	
+>=	    Greater than or equal to	    x >= y	
+<=	    Less than or equal to	        x <= y
+"""
+
+
+
+# Logical Operators 
+"""
+and 	Returns True if both statements are true                	    x < 5 and  x < 10	
+or	    Returns True if one of the statements is true   	            x < 5 or x < 4	
+not 	Reverse the result, returns False if the result is true	not     (x < 5 and x < 10)
+"""
