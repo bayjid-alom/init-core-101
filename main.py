2bc70f2d800396f4f9cdab9151b9ca9120883239
bayjidalom <bayjidalom1005@gmail.com>
2026-05-30 23:58:13 +0600

    Updated

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 # Day-0: Initialization Setup
+
+# What is Python?
+
+"""
+Python is a high-level, general-purpose programming language designed for readability and simplicity. Because its clean syntax closely resembles plain English, it is incredibly beginner-friendly and widely used for building websites, automating repetitive tasks, analyzing data, and powering artificial intelligence.
+
+"""
