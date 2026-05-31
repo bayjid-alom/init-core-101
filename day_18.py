d0d5df8bffae33a77259d4faf5effb337b701a93
bayjidalom <bayjidalom1005@gmail.com>
2026-05-31 23:57:27 +0600

    day-18: lists

---
+++ day_18.py
@@ -0,0 +1,31 @@
+# List  (mutable)
+
+"""
+List
+Lists are used to store multiple items in a single variable.
+Lists are one of 4 built-in data types in Python used to store collections of data,
+the other 3 are Tuple, Set, and Dictionary, all with different qualities and usage.
+
+### 4 built-in data
+1. data's
+2. tuple
+3. set
+4. dictionary
+
+"""
+
+# string
+fruits = ["Apple", "Banana", "Cherry"]
+print("Fruits :", fruits)
+
+# number
+numbers = [1, 2, 3,4, 5, 6, 7]
+print("Index (4) :", numbers[3])
+
+numbers[0] = 100
+print("Now list is :", numbers)
+
+# boolean
+boolean_value = [True, False, True]
+print("Type :", type(boolean_value))
+
