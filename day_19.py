7a8994fb235d97890e1e857b6a26fb485175e1af
bayjidalom <bayjidalom1005@gmail.com>
2026-06-01 17:42:53 +0600

    day-19: lists item access

---
+++ day_19.py
@@ -0,0 +1,12 @@
+# To access a list item in Python, you use square bracket notation [] with a zero-based index number
+
+name = ["Bayjid Alom", "Jubaeid Alom", "Babul Mia"]
+print(name)
+
+# item access
+print(name[1])
+print("my_name :", name[0])
+
+# item add
+name[0] = "Bayjid Alom Jihad"
+print("After Change :", name)
