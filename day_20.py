9292a2f7a18393af4105c07d23f481d41fb1abf7
bayjidalom <bayjidalom1005@gmail.com>
2026-06-01 17:50:38 +0600

    day-20: items add in list

---
+++ day_20.py
@@ -0,0 +1,30 @@
+
+# items add in list
+
+"""
+
+Append Items
+To add an item to the end of the list, use the append() method:
+
+Insert Items
+To insert a list item at a specified index, use the insert() method.
+"""
+
+# append method
+
+name = ["Bayjid Alom", "Jubaeid Alom", "Babul Mia"]
+print(name)
+name.append("Jihad")
+print(name)
+
+
+# insert method
+
+name.insert(0, "Position 0")
+print(name)
+
+name.insert(3, "Position 3")
+print(name)
+
+name.insert(2, "Position 2")
+print(name)
